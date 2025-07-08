fdfcdbdcf7819e3c390dc62895c7f612d6fd7a49
litenby <michaelshaffer@protonmail.com>
2025-07-08 17:28:13 -0400

    Updated README.md and added config files

---
+++ Program.cs
@@ -0,0 +1,482 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TriviaBotApp
+{
+    internal class TriviaBot
+    {
+        private Timer _messageTimer;
+        private Timer _questionTimer;
+        private Timer _commandTimer;
+        private Timer _startupMessageTimer;
+
+        private int _firstConnect = 0;
+        private string _nextPageToken;
+        private int _stage = 0;
+        private int _done = 1;
+        private string _currentQuestion = "";
+        private string _currentAnswer = "";
+        private int _currentQuestionLine = 0;
+        private string[] _recentMessages = new string[75];
+        private int _questionAnswered = 0;
+        private DateTime _askTime;
+        private string _hint1 = "";
+        private string _msgToSend = "";
+        private int _startUpMsgHoldBack = 1;
+        private readonly string _basePath = @"c:\triviabot\";
+        private string QuestionsFile => Path.Combine(_basePath, "questions.txt");
+        private string AnswersFile => Path.Combine(_basePath, "answers.txt");
+        private string ScoresFile => Path.Combine(_basePath, "scores.txt");
+        private string ClientSecretsFile => Path.Combine(_basePath, "client_secrets.json");
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trivia Bot");
+            Console.WriteLine("==========");
+
+            var triviaBot = new TriviaBot();
+            triviaBot.Start();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private void StackMessages(string input)
+        {
+            for (int i = 0; i < 74; i++)
+            {
+                _recentMessages[i + 1] = _recentMessages[i];
+            }
+            _recentMessages[0] = input;
+        }
+
+        public string GetScores()
+        {
+            var entries = File.ReadAllLines(ScoresFile);
+            var orderedEntries = entries.OrderByDescending(x => int.Parse(x.Split(',')[1]));
+
+            var topScores = orderedEntries.Take(5);
+            var highScores = "High scores: ";
+            foreach (var score in topScores)
+            {
+                highScores += score + " | ";
+            }
+
+            return highScores;
+        }
+
+        private void AddQuestion(string newQuestion)
+        {
+            var counter = 0;
+            string question = "";
+            string answer = "";
+            using (var file = new StreamReader(QuestionsFile))
+            {
+                while ((file.ReadLine()) != null)
+                {
+                    counter++;
+                }
+            }
+
+            var values = newQuestion.Split('#');
+            question = values[0].Substring(5);
+            answer = values[1];
+
+            File.AppendAllText(AnswersFile, answer + Environment.NewLine);
+            File.AppendAllText(QuestionsFile, question + Environment.NewLine);
+        }
+
+        private static void UpdateLine(string newText, string fileName, int lineToEdit)
+        {
+            var lines = File.ReadAllLines(fileName);
+            lines[lineToEdit] = newText;
+            File.WriteAllLines(fileName, lines);
+        }
+
+        public int GetUserScore(string userName)
+        {
+            int score = 0;
+            using (var file = new StreamReader(ScoresFile))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains(userName))
+                    {
+                        var values = line.Split(',');
+                        int.TryParse(values[1], out score);
+                    }
+                }
+            }
+
+            return score;
+        }
+
+        public void AddPoint(string userName)
+        {
+            int score = 0;
+            int foundLine = -1;
+            string replacement = "";
+            string newUserLine = "";
+            int destinationLine = 0;
+
+            using (var file = new StreamReader(ScoresFile))
+            {
+                string line;
+                int counter = 0;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains(userName))
+                    {
+                        foundLine = counter;
+                        var values = line.Split(',');
+                        int.TryParse(values[1], out score);
+                        score++;
+                        replacement = $"{values[0]},{score}";
+                    }
+                    counter++;
+                }
+            }
+
+            if (foundLine == -1)
+            {
+                newUserLine = $"{userName},1";
+                destinationLine = File.ReadAllLines(ScoresFile).Length + 1;
+                File.AppendAllText(ScoresFile, newUserLine + Environment.NewLine);
+            }
+            else
+            {
+                UpdateLine(replacement, ScoresFile, foundLine);
+            }
+        }
+
+        public void GenerateHints()
+        {
+            var sb = new StringBuilder(_currentAnswer);
+            for (int i = 1; i < sb.Length - 1; i++)
+            {
+                sb[i] = '_';
+            }
+            _hint1 = sb.ToString();
+        }
+
+        public void GetQuestion()
+        {
+            var lines = File.ReadAllLines(QuestionsFile);
+            var randomLineNumber = new Random().Next(0, lines.Length - 1);
+            _currentQuestion = lines[randomLineNumber];
+            _currentQuestionLine = randomLineNumber;
+        }
+
+        public void GetAnswer()
+        {
+            var answerLines = File.ReadAllLines(AnswersFile);
+            _currentAnswer = answerLines[_currentQuestionLine];
+        }
+
+        private void MessageTimer_Tick(object state)
+        {
+            try
+            {
+                GetMsg(_currentAnswer);
+                Console.WriteLine($"{DateTime.Now} - Retrieving new messages");
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+            }
+            GC.Collect();
+            Thread.Sleep(500);
+        }
+
+        private void CommandTimer_Tick(object state)
+        {
+            try
+            {
+                _startUpMsgHoldBack = 0;
+                Console.WriteLine("Messages are now allowed to be sent.");
+                _commandTimer.Change(Timeout.Infinite, Timeout.Infinite);
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+            }
+            GC.Collect();
+            Thread.Sleep(500);
+        }
+
+        private void StartupMessageTimer_Tick(object state)
+        {
+            try
+            {
+                SendMsg("Commands: !trivia !stop !myscore");
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+            }
+            GC.Collect();
+            Thread.Sleep(500);
+        }
+
+        private void QuestionTimer_Tick(object state)
+        {
+            if (_done == 1)
+            {
+                GetQuestion();
+                GetAnswer();
+                GenerateHints();
+                _stage = 0;
+                _done = 0;
+                _questionAnswered = 0;
+            }
+            else if (_stage == 0 && _done != 1)
+            {
+                _stage++;
+                _askTime = DateTime.Now;
+                try
+                {
+                    _msgToSend = $"{_currentQuestion}?";
+                    SendMsg(_msgToSend);
+                }
+                catch (AggregateException ex)
+                {
+                    foreach (var e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine($"Error: {e.Message}");
+                    }
+                }
+                GC.Collect();
+                Thread.Sleep(500);
+            }
+            else if (_stage == 1 && _done != 1)
+            {
+                _stage++;
+                try
+                {
+                    // SendMsg("hint 1");
+                }
+                catch (AggregateException ex)
+                {
+                    foreach (var e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine($"Error: {e.Message}");
+                    }
+                }
+                GC.Collect();
+                Thread.Sleep(500);
+            }
+            else if (_stage == 2 && _done != 1)
+            {
+                _stage++;
+                try
+                {
+                    SendMsg($"Hint: {_hint1}");
+                }
+                catch (AggregateException ex)
+                {
+                    foreach (var e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine($"Error: {e.Message}");
+                    }
+                }
+                GC.Collect();
+                Thread.Sleep(500);
+            }
+            else if (_stage == 3 && _done != 1)
+            {
+                _stage++;
+                try
+                {
+                    // SendMsg("hint 3");
+                }
+                catch (AggregateException ex)
+                {
+                    foreach (var e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine($"Error: {e.Message}");
+                    }
+                }
+                GC.Collect();
+                Thread.Sleep(500);
+            }
+            else if (_stage == 4 && _done != 1)
+            {
+                _stage = 0;
+                _done = 1;
+                try
+                {
+                    SendMsg($"Time is up! The correct answer was: {_currentAnswer}");
+                }
+                catch (AggregateException ex)
+                {
+                    foreach (var e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine($"Error: {e.Message}");
+                    }
+                }
+                GC.Collect();
+                Thread.Sleep(500);
+            }
+        }
+
+        private void Start()
+        {
+            _messageTimer = new Timer(MessageTimer_Tick, null, 3000, 2000);
+            _commandTimer = new Timer(CommandTimer_Tick, null, 15000, 15000);
+            _startupMessageTimer = new Timer(StartupMessageTimer_Tick, null, 30000, 3600000);
+        }
+
+        public async Task SendMsg(string message)
+        {
+            if (_startUpMsgHoldBack != 0)
+            {
+                Console.WriteLine($"HELD BACK: {message}");
+                return;
+            }
+
+            Console.WriteLine($"SENT: {message}");
+            UserCredential credential;
+
+            using (var stream = new FileStream(ClientSecretsFile, FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { YouTubeService.Scope.Youtube },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(this.GetType().ToString())
+                );
+            }
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var liveChatMessage = new LiveChatMessage
+            {
+                Snippet = new LiveChatMessageSnippet
+                {
+                    Type = "textMessageEvent",
+                    LiveChatId = "YOUR_LIVE_CHAT_ID",
+                    TextMessageDetails = new LiveChatTextMessageDetails
+                    {
+                        MessageText = message
+                    }
+                }
+            };
+
+            await youtubeService.LiveChatMessages.Insert(liveChatMessage, "snippet").ExecuteAsync();
+        }
+
+        public async Task GetMsg(string curAnswer)
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream(ClientSecretsFile, FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { YouTubeService.Scope.Youtube },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(this.GetType().ToString())
+                );
+            }
+
+            var ytService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = this.GetType().ToString()
+            });
+
+            string liveChatId = "YOUR_LIVE_CHAT_ID";
+            var chatRequest = ytService.LiveChatMessages.List(liveChatId, "id,snippet,authorDetails");
+            chatRequest.PageToken = _nextPageToken;
+
+            var chatResponse = await chatRequest.ExecuteAsync();
+            _nextPageToken = chatResponse.NextPageToken;
+
+            foreach (var message in chatResponse.Items)
+            {
+                var messageId = message.Id;
+                var displayName = message.AuthorDetails.DisplayName;
+                var displayMessage = message.Snippet.DisplayMessage;
+                var messageTime = message.Snippet.PublishedAt ?? DateTime.MinValue;
+
+                if (displayName != "Trivia Bot" &&
+                    !_recentMessages.Contains(messageId) &&
+                    _startUpMsgHoldBack == 0)
+                {
+                    StackMessages(messageId);
+                    Console.WriteLine($"{DateTime.Now} Received: {displayMessage}");
+
+                    if (displayMessage.IndexOf(curAnswer, StringComparison.OrdinalIgnoreCase) >= 0 &&
+                        _done == 0 &&
+                        _questionAnswered == 0)
+                    {
+                        _questionAnswered = 1;
+                        _done = 1;
+
+                        var delay = (DateTime.Now - messageTime).Seconds;
+                        await SendMsg($"You got it, {displayName}! [{delay}s] The correct answer was: {curAnswer}.");
+                        AddPoint(displayName);
+                    }
+                    else if (displayMessage.Contains("!trivia"))
+                    {
+                        _done = 1;
+                        _stage = 0;
+                        await SendMsg("Trivia Bot started! First question coming up...");
+                        _questionTimer = new Timer(QuestionTimer_Tick, null, 0, 10000);
+                    }
+                    else if (displayMessage.Contains("!stop"))
+                    {
+                        _done = 1;
+                        await SendMsg($"Trivia stopped by {displayName}");
+                        _questionTimer?.Change(Timeout.Infinite, Timeout.Infinite);
+                    }
+                    else if (displayMessage.Contains("!myscore"))
+                    {
+                        var score = GetUserScore(displayName);
+                        await SendMsg($"{displayName}'s score: {score}");
+                    }
+                    else if (displayMessage.Contains("!add"))
+                    {
+                        AddQuestion(displayMessage);
+                        await SendMsg("Question added.");
+                    }
+                    else if (displayMessage.Contains("!highscores"))
+                    {
+                        var scores = GetScores();
+                        await SendMsg(scores);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,117 @@
+﻿# Youtube Trivia Bot  
+  
+A trivia game for Youtube live stream chats that runs on a Windows computer.   
+  
+--------------------------------------------------------------------------------  
+Features:  
+--------------------------------------------------------------------------------   
+- Responds to commands in the youtube chat.  
+- Questions and answers are stored in two text files on admin's computer.  
+- Questions can be added in the chatroom, by moderators only if enabled.  
+  
+--------------------------------------------------------------------------------  
+Quick Start:  
+--------------------------------------------------------------------------------  
+1. Download and extract the program files.  
+2. Set up Google OAuth2 credentials (see detailed instructions below).  
+3. Copy appsettings.template.json → appsettings.json  
+4. Edit appsettings.json and add your YouTube LiveChat ID.  
+5. Run YoutubeSpamFilter.exe  
+  
+--------------------------------------------------------------------------------  
+Program Setup:  
+--------------------------------------------------------------------------------  
+1. Ensure Microsoft .NET Framework 4.6.1 is installed.  
+2. Create a folder for the program (e.g., C:\spamfilter).  
+3. Extract the program files into that folder.  
+4. Rename appsettings.template.json to appsettings.json  
+5. Edit appsettings.json and paste in your LiveChatID value.  
+6. Save the file.  
+7. Place client_secret.json into the same directory as appsettings.json  
+
+
+--------------------------------------------------------------------------------  
+Google OAuth2 Setup:  
+--------------------------------------------------------------------------------  
+1. Sign in to Google using the account that will run the bot (must be a moderator or owner).  
+2. Search for "YouTube API Console" in Google.  
+3. Open the "YouTube Data API Overview - Google Developers" link.  
+4. In the left sidebar, click "Get Auth Credentials".  
+5. Click "Open the Credentials page".  
+6. Click "Create" to make a new project.  
+7. Accept the terms of service.  
+8. Name your project (any name is fine), then click Create.  
+9. Click "Create Credentials" > "OAuth client ID".  
+10. Click "Configure consent screen".  
+11. Enter a product name (can be anything).  
+12. Leave other fields blank and click Save.  
+13. Under "Create OAuth client ID", select "Other" (or "Desktop App" if "Other" is unavailable).  
+14. Name it (e.g., "My Client") and click Create.  
+15. On the success screen, click OK.  
+16. Click the download icon next to your new client to download the credentials.  
+17. Rename the downloaded file to: client_secret.json  
+18. Go to API & Services > Dashboard  
+19. Click "Enable APIs and Services"  
+20. Search for "YouTube Data API v3", click it, then click Enable.  
+21. Ensure you're still signed into the correct Google account (top-right corner).  
+	
+--------------------------------------------------------------------------------  
+Get Your YouTube LiveChat ID:  
+--------------------------------------------------------------------------------  
+1. Go to the YouTube API Explorer.  
+2. Make sure you are signed in as the channel owner.  
+3. Click youtube.liveBroadcasts.list  
+4. In the "part" field, enter: snippet  
+5. In "broadcastStatus", select: active (or upcoming)  
+6. Click Execute.  
+7. In the response, find and copy the value of: liveChatId  
+
+--------------------------------------------------------------------------------  
+Commands:
+-------------------------------------------------------------------------------- 
+!Trivia - start the game    
+!stop - Stop the game  
+!myscore - Show your score  
+!add question#answer - Add a question (if allowed)   
+!highscores - List top scores  
+
+
+
+
+--------------------------------------------------------------------------------  
+Customize the Configuration File:
+-------------------------------------------------------------------------------- 
+Open YoutubeTriviaBot.exe.config   
+Edit the following values. Use double backslashes for directory names. Also might want to use a directory with a short simple path  
+with no spaces or special characters, etc.   
+  
+Add questions and answers to the questions.txt and answers.txt.  
+The question and answer should be on the same line in each file.  
+Don't put question mark at the end of the questions. The bot adds the question mark.   
+The cursor should be on the next line at the end of the document. There shouldn't be any extra lines.  
+Using NotePad++ seems to show more if there are hidden lines or text that could interfere with reading   
+the file.   
+
+questionsLocation - location to the questions.txt  
+answersLocation - location to the answers.txt  
+scoresLocation - location to the scores.txt  
+clientSecretsLocation - location to the client_secrets.json  
+getMsgDelay - how often in seconds that the bot retrieves chat messages from youtube  
+questionDelay - time period to answer question  
+advertiseDelay - how long in seconds between advertising the trivia bot's commands in the chat  
+livechatid - the unique id of the chat found in step 2  
+botName - set this to match the name of the bot's user name, for example Trivia Bot. This is so the bot ignores messages from itself in the chat.  
+restrictedAdd - set to "yes" and only the name in the questionAuthor field will be able to add questions. Set to "no" and anyone can use the add question command  
+questionAuthor - the bot will only add questions from this user name if restrictedAdd is set to "yes"  
+  
+--------------------------------------------------------------------------------  
+Run the Program:  
+-------------------------------------------------------------------------------- 
+8) Double click the YoutubeTriviaBot.exe to run the program  
+After about 10 seconds it will finish startup  
+If you have the correct LiveChatID you should see messages in the console window  
+when someone types something in the chat  
+There aren't any commands that can be entered in the console, everything is done  
+in the chat.   
+
+![Screenshot of the trivia bot running in the chat](images/screenshot1.png)
